2e9fd8cbabb9ac37587c94985de1d6a04334b0b8
hugohil <hilairehugo@gmail.com>
2014-12-07 21:36:18 +0100

    control distant player

--- js/remote.js
+++ js/remote.js
@@ -1,15 +1,18 @@
 var socket = io('http://localhost:1337');
-var front = io('http://localhost:1337/remote');
+var remote = io('http://localhost:1337/remote');
+var player = io('http://localhost:1337/player');
 var songs = songs || [];
 var stack = stack || [];
-var delivery = new Delivery(front);
+var delivery = new Delivery(remote);
 
 socket.on('set-library', function (library){
+  songs = [];
+  $('#playlist li').remove();
   for(var i = 0; i < library.length; i++){
     songs.push(library[i]);
     addToList('#playlist', library[i]);
   }
-  init();
+  init(); // display message while library is not loaded
 });
 
 socket.on('new-song', function (name){
@@ -18,6 +21,38 @@ socket.on('new-song', function (name){
 })
 
 function init(){
+  var prev = document.getElementById('prev');
+  var next = document.getElementById('next');
+  var play = document.getElementById('play');
+  var rand = document.getElementById('rand');
+  var auto = document.getElementById('auto');
+
+  play.onclick = function (){
+   player.emit('change-state');
+  }
+  prev.onclick = function (){
+   player.emit('change-song', 'prev');
+  }
+  next.onclick = function (){
+   player.emit('change-song', 'next');
+  }
+  rand.onclick = function (){
+    player.emit('switch-random');
+  }
+  auto.onclick = function (){
+    player.emit('switch-auto');
+  }
+
+  $('body').keyup(function (event){
+    if(event.keyCode == 32){
+      player.emit('change-state');
+    } else if(event.keyCode == 39) {
+      player.emit('change-song', 'next');
+    } else if(event.keyCode == 37){
+      player.emit('change-song', 'prev');
+    }
+  })
+
   $('.js-add-to-stack').click(function (event){
     var input = $('.js-file')[0];
     var file = input.files[0];
